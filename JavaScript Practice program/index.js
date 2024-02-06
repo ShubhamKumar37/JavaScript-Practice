90bf47a270d26914127c2609857ce6e55e9a52f6
ShubhamKumar37 <sk9818281820@gmail.com>
2024-02-06 14:24:53 +0530

    New Topic

--- JavaScript Practice program/index.js
+++ JavaScript Practice program/index.js
@@ -10,3 +10,9 @@ const Arr = 34;
 // Arr = 34;
 // Arr = 32;
 console.log(Arr, "this value is constant and we cann't change its value if we try to do so then it will result as error");
+
+var D = undefined;
+D = 'Shubham';
+console.log(D);
+D = 123;
+console.log(D);
