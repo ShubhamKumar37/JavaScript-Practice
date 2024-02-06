ef0fe89fb7be3511a71689e36ccaebfac0839d13
ShubhamKumar37 <sk9818281820@gmail.com>
2024-02-06 13:07:16 +0530

    basic Java Script statement and program

--- JavaScript Practice program/index.js
+++ JavaScript Practice program/index.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+let A = 10;
+var B = 10;
+var B = 190;
+console.log(A + 123 + " Haa haa");
+
+const Arr = 34;
+// console.log(Arr, "this value is constant and we cann't change its value if we try to do so then it will result as error");
+// Arr = 34;
+// Arr = 32;
+console.log(Arr, "this value is constant and we cann't change its value if we try to do so then it will result as error");
