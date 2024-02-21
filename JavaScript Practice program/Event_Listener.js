63523773f8b212dbeb26e1757f21bcccb7e61e38
ShubhamKumar37 <sk9818281820@gmail.com>
2024-02-22 01:28:45 +0530

    JavaScript Basics

--- JavaScript Practice program/Event_Listener.js
+++ JavaScript Practice program/Event_Listener.js
@@ -1,25 +1,47 @@
-function Event()
-{
-    console.log("He you click on dark web stuff");
-}
-var Count = 0;
-document.addEventListener('keydown', Event);
-// document.removeEventListener('click', Event);
-// Count++;
-// if(Count > 4)
+// function Event()
 // {
+//     console.log("He you click on dark web stuff");
+// }
+// var Count = 0;
+// document.addEventListener('keydown', Event);
+
+// let Content = document.querySelector('#Para');
+
+// Content.addEventListener('dblclick', function(Event)
+// {
+//     console.log(Event);
+// });
+
+// let Links = document.querySelector('a');
+// Links.addEventListener('click', function(Event)
+// {
+//     Event.preventDefault();
+//     console.log("Ops, we delete the default action");
+// })
+
+// let My_Div = document.createElement('div');
+
+// function Event_Para(Event)
+// {
+//     console.log(Event.target.textContent);
 // }
 
-// Creating a new custom event
-const customEvent = new Event('customEvent');
+// for(let i = 1; i <= 100; i++)
+// {
+//     let New_Tag = document.createElement('p');
+//     New_Tag.textContent = 'Hi this is para ' + i; 
+    
+//     New_Tag.addEventListener('click', Event_Para);
+//     My_Div.appendChild(New_Tag);
+// }
 
-// Getting the target element (could be any DOM element)
-const targetElement = document.getElementById('script');
+// document.body.appendChild(My_Div);
 
-// Adding an event listener to the target element
-targetElement.addEventListener('customEvent', function(event) {
-    console.log('Custom event triggered!');
+let Content = document.querySelector('.Check_Portion_Event');
+Content.addEventListener('click', function(Event)
+{
+    if(Event.target.nodeName == 'SPAN')
+    {
+        console.log('You Touched ' + Event.target.textContent);
+    }
 });
-
-// Dispatching the custom event on the target element
-targetElement.dispatchEvent(customEvent);
