7c50918bc7ccf2d5c55f9e1fd1eef7166009d58e
ShubhamKumar37 <sk9818281820@gmail.com>
2024-03-01 00:17:18 +0530

    JavaScript exercise

---
+++ JavaScript Exercise/JS_Exercise1.js
@@ -0,0 +1,62 @@
+// Q1
+// function Add(A, B)
+// {
+//     return A + B;
+// }
+
+// let Add = (A, B) => {return A + B};
+
+// console.log(Add(234,123));
+
+// Q2
+// function Length_Str(Str)
+// {
+//     return Str.length;
+// }
+
+// let Length_Str = (Str) => {return Str.length};
+
+// console.log(Length_Str("Shubham"));
+
+// Q3
+// function Diff_Operator_Mani(A, B)
+// {
+//     console.log(A + B);
+//     console.log(A - B);
+//     console.log(A * B);
+//     console.log(A / B);
+// }
+
+// let Diff_Operator_Mani = (A, B) =>
+// {
+//     console.log(A + B);
+//     console.log(A - B);
+//     console.log(A * B);
+//     console.log(A / B);
+// }
+
+// Diff_Operator_Mani(100, 34);
+
+// Q4
+// function Max_Int(A, B)
+// {
+//     return A > B ? A : B;
+// }
+
+// console.log("Max value is " + Max_Int(342, 3432));
+
+// Q5
+// function Reverse(Str)
+// {
+//     return Str.split('').reverse().join('');
+// }
+
+// console.log(Reverse("Shubham Kumar"));
+
+// Q6
+// function Check_Int(A)
+// {
+//     return A > 0 ? "Number greater then zero" : ((A < 0) ? "Number lesser then zero" : "Number is equal to zero");
+// }
+
+// console.log(Check_Int(-234));
