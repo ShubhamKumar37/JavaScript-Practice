ef0b3af5fbb25f54109b300d31702fe650f3e70e
ShubhamKumar37 <sk9818281820@gmail.com>
2024-03-03 17:16:48 +0530

    Passwor Generator 15% minor project

--- Minor_Project_Js/Password_Generator/Password_Generator.js
+++ Minor_Project_Js/Password_Generator/Password_Generator.js
@@ -10,3 +10,13 @@ let Symbol = document.querySelector('#Symbol');
 let Strength_Indicator = document.querySelector('[Strength_Indicator]');
 let Generate_Password = document.querySelector('[Generate_Password]');
 let All_Check_Box = document.querySelectorAll('input[type=checkbox]');
+
+let Pass_Length = 4;
+Change_Password_Length_Slider();
+
+
+function Change_Password_Length_Slider()
+{
+    Password_length_Number.innerText = Pass_Length;
+    Password_length_Slider.value = Pass_Length;
+}
