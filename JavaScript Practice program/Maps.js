3004b4ce957c8a6c0a1fb5e0c960655feefea293
ShubhamKumar37 <sk9818281820@gmail.com>
2024-03-11 00:52:15 +0530

    Js Map Revise

--- JavaScript Practice program/Maps.js
+++ JavaScript Practice program/Maps.js
@@ -6,9 +6,14 @@ let Arr = [3, 5, 2, -7, -8];
 // console.log(Mapped );
 
 // Chaining
-let Mapped_Obj = Arr.filter(Val => Val >= 0).map((Value) =>
-{
-    return {Value : Value};
-});
+// let Mapped_Obj = Arr.filter(Val => Val >= 0).map((Value) =>
+// {
+//     return {Value : Value};
+// });
+
+let Mapped_Obj = Arr.map((Value, index, array) => {
+    console.log(Value, index, array);
+    return Value;
+})
 
 console.log(Mapped_Obj);
